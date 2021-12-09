92eff11a84e9f8f7e0ef9123ae2990ee8a8fd15c
Firas Krifa <firaskrifa02@gmail.com>
2021-12-09 14:44:56 +0100

    copie 140

--- src/main/java/tn/esprit/CallRestWebService.java
+++ src/main/java/tn/esprit/CallRestWebService.java
@@ -21,6 +21,11 @@ import org.json.JSONObject;
 
 public class CallRestWebService {
 // new commit
+	// modifier 5 
+	// modifier 78 
+	// copie 78
+	
+		// copie 140
 	public static final String endpoint = "http://ip-api.com/json";
 	//public static final String endpoint = "https://httpbin.org/get";
 	public static void main(String[] args) {
